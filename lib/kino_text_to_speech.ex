e215299c82e55ebc3428df8ff3c31dbbc93e6edf
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-10-01 15:40:09 -0400

    Elixir v1.15.6 and Erlang 26.1.1
    Ran `mix new kino_text_to_speech --sup` to spin up a new project

---
+++ lib/kino_text_to_speech.ex
@@ -0,0 +1,18 @@
+defmodule KinoTextToSpeech do
+  @moduledoc """
+  Documentation for `KinoTextToSpeech`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KinoTextToSpeech.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kino_text_to_speech_test.exs
@@ -0,0 +1,8 @@
+defmodule KinoTextToSpeechTest do
+  use ExUnit.Case
+  doctest KinoTextToSpeech
+
+  test "greets the world" do
+    assert KinoTextToSpeech.hello() == :world
+  end
+end
