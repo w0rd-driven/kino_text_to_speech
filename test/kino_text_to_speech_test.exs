5ff6539870e2a0d3c59f29e8fbf6cba8ec859a55
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-10-08 02:01:59 -0400

    Add test action

--- test/kino_text_to_speech_test.exs
+++ test/kino_text_to_speech_test.exs
@@ -1,8 +1,26 @@
 defmodule KinoTextToSpeechTest do
-  use ExUnit.Case
-  doctest KinoTextToSpeech
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert KinoTextToSpeech.hello() == :world
+  import Kino.Test
+
+  alias KinoTextToSpeech.TextToSpeechCell
+
+  setup :configure_livebook_bridge
+
+  describe "initialization" do
+    test "restores source code from attrs" do
+      attrs = %{
+        "pitch" => 1.0,
+        "rate" => 1.0,
+        "text" => "Greetings Professor Falken",
+        "voice" => "Ralph",
+        "volume" => 1.0
+      }
+
+      {_kino, source} = start_smart_cell!(TextToSpeechCell, attrs)
+
+      assert source ==
+               "%Kino.Markdown{content: \"**Speaking**:  Greetings Professor Falken\"}"
+    end
   end
 end
